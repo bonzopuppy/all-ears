8ac79991fc0f6b55d3eb59fa7d6688ca33827476
David Baden <davebaden@gmail.com>
2023-12-07 17:38:34 -0800

    small fixes

--- src/components/SongSmall.js
+++ src/components/SongSmall.js
@@ -18,6 +18,7 @@ function SongSmall({release}) {
                 display: 'flex',
                 alignItems: 'center',
                 position: 'relative',
+                height: 76,
                 '&:hover': {
                     backgroundColor: 'rgba(24, 28, 30, 0.08)', // Background color on hover
                     cursor: 'pointer',
@@ -33,7 +34,7 @@ function SongSmall({release}) {
 
                 {/* Album Image */}
                 <Box sx={{ width: '64px', height: '64px', marginRight: '10px', marginLeft: '10px', position: 'relative' }}>
-                    <img src={release.images[0].url} alt="Album" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
+                    <img src={release.images[0].url} alt="Album" style={{ width: '100%', height: '100%', objectFit: 'cover', borderRadius: 4 }} />
                     <Box className="playHover"
                         sx={{
                             position: 'absolute',
@@ -56,7 +57,7 @@ function SongSmall({release}) {
 
                 {/* Song Info */}
                 <Box>
-                    <Typography variant="subtitle1" sx={{ fontWeight: '500' }}>
+                    <Typography variant="subtitle1" sx={{ fontWeight: '600' }}>
                         {release.name}
                     </Typography>
                     <Typography variant="subtitle2" sx={{ color: 'text.secondary' }}>
